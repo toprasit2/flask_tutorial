6d25e41228082a92bd97e580e5f0110f5dfa794c
toprasit2 <toprasit.gk@gmail.com>
2018-05-17 08:21:15 -0700

    if rendertemplate

--- app.py
+++ app.py
@@ -2,15 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Thanaphon Toprasit',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'April 21, 2018'
+    },
+    {
+        'author': 'John Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'April 22, 2018'
+    }
+ ]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "Hello About"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
